76a69f6ae79baf0dbb9ae797ee05ef6c2e96ab36
Jacek Winiarczyk <kancelariajkw@gmail.com>
2023-03-02 22:44:24 +0100

    Disk fixed

--- disk.asm
+++ disk.asm
@@ -3,7 +3,7 @@ bits 16
 push	ebp
 mov		ebp,	esp
 
-push	WORD[ebp+8]
+push	WORD[ebp+8]				; print parameter
 call	putc
 add		sp,		2
 
@@ -11,7 +11,7 @@ jmp		start
 db "DISK INFORMATION: ABC",0x00
 
 start:
-push	DWORD START_TEXT
+push	DWORD DISK_TEXT
 call	puts
 add		sp,		4
 
@@ -19,14 +19,11 @@ int		0x20
 
 ;;;;;;;;;;;;;;;;;;
 ;; return procedure
-mov		bx,		[ebp+6]
-mov		es,		bx
-mov		bx,		0x0
-
 ; change segment
-mov		bx,		[ebp+4]			; offset
 mov		ax,		[ebp+6]			; segment
-pop		ebp						; get previous frame pointer
+mov		bx,		[ebp+4]			; offset
+pop		ebp						; reset ebp as it was
+mov		esp,	ebp				; reset stack as it was
 mov		ds,		ax
 mov		es,		ax
 mov		fs,		ax
@@ -34,15 +31,14 @@ mov		gs,		ax
 mov		ss,		ax
 
 ; jump back
-mov		[cs:RETURN_ADDRESS],	bx	; offset
-mov		[cs:RETURN_ADDRESS+2], 	ax	; segment
-mov		esp,	ebp				; reset stack as it was
-jmp		far [cs:RETURN_ADDRESS]
+push	0
+push	ax						; segment
+push	bx						; offset
+iret
 hlt
 
 %include "boot/io.asm"
 
-START_TEXT db 0x0a,0x0d,"Disk loaded successfully",0x0a,0x0d,0x00
-RETURN_ADDRESS dw 0x0,0x0
+DISK_TEXT db 0x0a,0x0d,"Disk loaded successfully",0x0a,0x0d,0x00
 
 times 512 - ($-$$) db 0x90
